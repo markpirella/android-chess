4662e82a22b33afc641aa4672dd025c8ceb9498d
nickf99 <64861874+nickf99@users.noreply.github.com>
2020-12-07 18:52:24 -0500

    more work on watch game list

---
+++ app/src/main/java/com/example/chess69/GameListAdapter.java
@@ -0,0 +1,49 @@
+package com.example.chess69;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Color;
+import android.text.Layout;
+import android.view.DragEvent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.TextView;
+import com.example.chess69.MainActivity;
+
+import java.util.ArrayList;
+
+public class GameListAdapter extends ArrayAdapter<SavedGame> {
+    private final Activity context;
+    private final ArrayList<String> titles;
+    private final ArrayList<SavedGame> savedGames;
+
+    public GameListAdapter(Activity context, ArrayList<SavedGame> games) {
+        super(context, R.layout.game_list_entry, games);
+        this.context = context;
+        this.titles = null;
+        this.savedGames = null;
+        for (SavedGame savedGame : games) {
+            this.savedGames.add(savedGame);
+        }
+        for (SavedGame s : games) {
+            titles.add(s.title);
+        }
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = context.getLayoutInflater();
+        View rowView = inflater.inflate(R.layout.game_list_entry, null, true);
+
+        TextView gameTitle = (TextView) rowView.findViewById(R.id.game_entry);
+        gameTitle.setText(titles.get(position));
+
+        return rowView;
+    }
+
+}
--- app/src/main/java/com/example/chess69/WatchGame.java
+++ app/src/main/java/com/example/chess69/WatchGame.java
@@ -1,16 +1,45 @@
 package com.example.chess69;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
+
 public class WatchGame extends AppCompatActivity {
 
-    //private ListView gameListView;
-    private String[] gameNames;
+    private ListView gameListView;
+    //private String[] gameNames;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_watch_game);
+
+        SavedGames savedGamesObj;
+        try {
+            ObjectInputStream ois = new ObjectInputStream(new FileInputStream(getApplicationInfo().dataDir + File.separator + "SavedGames.dat"));
+            savedGamesObj = (SavedGames) ois.readObject();
+            ois.close();
+
+            GameListAdapter adapter = new GameListAdapter(this, savedGamesObj.games);
+            gameListView = (ListView) findViewById(R.id.game_list);
+            gameListView.setAdapter(adapter);
+
+            gameListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    Intent intent = new Intent(view.getContext(), WatchSavedGame.class);
+                }
+            });
+        }catch(Exception e) {
+            e.printStackTrace();
+        }
     }
 }
